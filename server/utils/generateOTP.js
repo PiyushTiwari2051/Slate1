a7cc9af61754d83b8d2bea4534f0c91b40049be8
PIY2052 <piyushtiwari2051@gmail.com>
2026-05-25 14:05:54 +0530

    Update OTP generator to return static code in development mode

--- server/utils/generateOTP.js
+++ server/utils/generateOTP.js
@@ -1,6 +1,10 @@
 import crypto from 'crypto';
 
 export const generateOTP = () => {
+  // Use static OTP in development for ease of local signup testing
+  if (process.env.NODE_ENV === 'development') {
+    return '123456';
+  }
   // Cryptographically secure 6-digit OTP
   return String(crypto.randomInt(100000, 999999));
 };
